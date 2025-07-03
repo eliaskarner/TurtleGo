f5586fc1c6787c7ea44fb2d5ccd12b13dc8a0c6b
eliaskarner <131982229+eliaskarner@users.noreply.github.com>
2025-07-03 22:46:06 +0200

    Update setup.py

--- turtlesimAutomata/setup.py
+++ turtlesimAutomata/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/turtle_start.launch.py']),
     ],
     install_requires=['setuptools','rclpy', 'turtlesim', 'geometry_msgs'],
     zip_safe=True,
